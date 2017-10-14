42611b4b46cea33e5e4ed9352ab6bd6c33cd390c
Nuralamsah Zulkarnaim <30795934+alamsahzul@users.noreply.github.com>
2017-10-14 19:42:49 +0700

    Update num.js

--- num.js
+++ num.js
@@ -2,8 +2,11 @@ function sum(){
     var i, sum = 0;
     for(i = 0; i < arguments.length; i++) {
         sum += arguments[i];
+        if(i==arguments.length){
+            console.log('This is the end')
+        }
     }
-console.log(sum);
+console.log('Total Sum: ',sum);
 }
 
 sum(1,2,7)
