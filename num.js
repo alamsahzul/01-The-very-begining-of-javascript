be3481e6805dbde7492cff40b1e2d4bbbc5581fc
Nuralamsah Zulkarnaim <30795934+alamsahzul@users.noreply.github.com>
2017-10-20 19:59:00 +0700

    Update num.js

--- num.js
+++ num.js
@@ -1,6 +1,6 @@
 function sum(){ 
     var i, sum = 0;
-    for(i = 0; i < arguments.length; i++) {
+    for(i = 0; i < arguments.length; i++) { 
         sum += arguments[i];
         if(i==arguments.length){
             console.log('This is the end')
